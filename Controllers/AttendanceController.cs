c92e8bb64d3ee3b0044fabeb82f49b66eb28b287
Ciucur Daniel <ciucur.daniel14@gmail.com>
2021-01-11 02:26:43 +0200

    I seprate the solution on layers on 3 projects

---
+++ Controllers/AttendanceController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SportAttendanceManager.DataAccessLayer;
+using SportAttendanceManager.Models;
+
+namespace SportAttendanceManager.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        private SportAttendanceManagerContext db = new SportAttendanceManagerContext();
+
+        // GET: Attendance
+        public ActionResult Index()
+        {
+            var attendances = db.Attendances.Include(a => a.Student);
+            return View(attendances.ToList());
+        }
+
+        // GET: Attendance/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attendance attendance = db.Attendances.Find(id);
+            if (attendance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attendance);
+        }
+
+        // GET: Attendance/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdStudent = new SelectList(db.Students, "IdStudent", "FirstName");
+            return View();
+        }
+
+        // POST: Attendance/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdAttendance,ReportDate,IdStudent")] Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Attendances.Add(attendance);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdStudent = new SelectList(db.Students, "IdStudent", "FirstName", attendance.IdStudent);
+            return View(attendance);
+        }
+
+        // GET: Attendance/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attendance attendance = db.Attendances.Find(id);
+            if (attendance == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdStudent = new SelectList(db.Students, "IdStudent", "FirstName", attendance.IdStudent);
+            return View(attendance);
+        }
+
+        // POST: Attendance/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdAttendance,ReportDate,IdStudent")] Attendance attendance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(attendance).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdStudent = new SelectList(db.Students, "IdStudent", "FirstName", attendance.IdStudent);
+            return View(attendance);
+        }
+
+        // GET: Attendance/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attendance attendance = db.Attendances.Find(id);
+            if (attendance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attendance);
+        }
+
+        // POST: Attendance/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Attendance attendance = db.Attendances.Find(id);
+            db.Attendances.Remove(attendance);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/SportController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SportAttendanceManager.DataAccessLayer;
+using SportAttendanceManager.Models;
+
+namespace SportAttendanceManager.Controllers
+{
+    public class SportController : Controller
+    {
+        private SportAttendanceManagerContext db = new SportAttendanceManagerContext();
+
+        // GET: Sport
+        public ActionResult Index()
+        {
+            // select the id of the logged in teacher
+            var id = Session["idUser"];
+
+                var sports = db.Sports.Include(s => s.User);
+                return View(sports.ToList());
+
+        }
+
+        // GET: Sport/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sport sport = db.Sports.Find(id);
+            if (sport == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sport);
+        }
+
+        // GET: Sport/Create
+        public ActionResult Create()
+        {
+            ViewBag.idUser = new SelectList(db.Users, "idUser", "FirstName");
+            return View();
+        }
+
+        // POST: Sport/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdSport,Hour,Minute,Description,idUser")] Sport sport)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sports.Add(sport);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idUser = new SelectList(db.Users, "idUser", "FirstName", sport.idUser);
+            return View(sport);
+        }
+
+        // GET: Sport/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sport sport = db.Sports.Find(id);
+            if (sport == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idUser = new SelectList(db.Users, "idUser", "FirstName", sport.idUser);
+            return View(sport);
+        }
+
+        // POST: Sport/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdSport,Hour,Minute,Description,idUser")] Sport sport)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sport).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idUser = new SelectList(db.Users, "idUser", "FirstName", sport.idUser);
+            return View(sport);
+        }
+
+        // GET: Sport/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sport sport = db.Sports.Find(id);
+            if (sport == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sport);
+        }
+
+        // POST: Sport/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sport sport = db.Sports.Find(id);
+            db.Sports.Remove(sport);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
